fcf798799f29a87c9a904c1ad6811274fdebc109
ThePupilOfTheOcean <pupilocean@gmail.com>
2018-04-07 23:02:27 +0800

    修复无法访问主页

--- controller/utilities/index.go
+++ controller/utilities/index.go
@@ -10,11 +10,6 @@ import (
 // GetIndex 首页
 func GetIndex(c *gin.Context) {
 	var notifies []database.Notify
-
-	if dbe := database.DB.Find(&notifies); dbe != nil {
-		c.AbortWithStatusJSON(http.StatusOK, gin.H{"status": 1, "msg": dbe.Error.Error()})
-		return
-	}
-
+	database.DB.Find(&notifies)
 	c.HTML(http.StatusOK, "common/index.tmpl", gin.H{"notifies": notifies})
 }
