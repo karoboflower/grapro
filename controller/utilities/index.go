18abb1de1ad52e9b0aceeeda980de2d25de125f6
ThePupilOfTheOcean <pupilocean@gmail.com>
2018-04-12 00:42:45 +0800

    测试.html

--- controller/utilities/index.go
+++ controller/utilities/index.go
@@ -12,5 +12,5 @@ func GetIndex(c *gin.Context) {
 	var notifies []database.Notify
 	database.DB.Find(&notifies)
 	// c.HTML(http.StatusOK, "common/index.tmpl", gin.H{"notifies": notifies})
-	c.JSON(http.StatusOK, gin.H{"notifies": notifies})
+	c.HTML(http.StatusOK, "common/index.html", gin.H{"notifies": notifies})
 }
